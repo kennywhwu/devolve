1b69ca11e2f62ab5f1b96484e551435324238823
Kenny Hwu <kennywhwu@gmail.com>
2018-11-01 16:10:04 -0700

    set up board

--- src/Board.js
+++ src/Board.js
@@ -7,33 +7,39 @@ class Board extends Component {
     this.state = { board: this.createBoard() };
   }
   static defaultProps = {
-    dimension: 3
+    xDimension: 10,
+    yDimension: 6
   };
 
   createBoard() {
-    let board = Array.from({ length: this.props.dimension }).map((e1, y) => {
-      return Array.from({ length: this.props.dimension }).map((e2, x) => {
+    let board = Array.from({ length: this.props.yDimension }).map((e1, y) => {
+      return Array.from({ length: this.props.xDimension }).map((e2, x) => {
         return 0;
       });
     });
+    console.log('board ', board);
     return board;
   }
 
   render() {
     let tblBoard = [];
-    for (let y = 0; y < this.props.dimension; y++) {
+    for (let y = 0; y < this.props.yDimension; y++) {
       let row = [];
-      for (let x = 0; x < this.props.dimension; x++) {
+      for (let x = 0; x < this.props.xDimension; x++) {
         let coord = `${y}-${x}`;
-        row.push(<td className="cell" />);
+        row.push(<td className="cell" key={coord} coord={coord} />);
       }
-      tblBoard.push(<tr key={y}>{row}</tr>);
-      return (
-        <table className="Board">
-          <tbody>{tblBoard}</tbody>
-        </table>
+      tblBoard.push(
+        <tr key={y} row={y}>
+          {row}
+        </tr>
       );
     }
+    return (
+      <table className="Board">
+        <tbody>{tblBoard}</tbody>
+      </table>
+    );
   }
 }
 
